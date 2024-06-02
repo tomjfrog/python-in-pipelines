370ef8025063402557d7df06b49c1228709d582c
tomjfrog <tomj@jfrog.com>
2024-06-02 15:13:02 -0500

    YOLO

--- test.py
+++ test.py
@@ -4,9 +4,10 @@
 # import base64
 # import json
 import logging
-# import os
-import subprocess
+import os
 import requests
+
+
 # import sys
 
 def main():
@@ -14,12 +15,16 @@ def main():
         format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s: %(message)s",
         level=logging.DEBUG
     )
+    basic_auth_string = os.environ['int_webhook_basic_auth_b64_encoded_basic_auth_string']
     logging.info("Starting Python Script...")
-    test_curl = subprocess.run('curl -X POST https://tomjfrog-pipelines-api.jfrog.io/v1/projectIntegrations/127/hook --header Authorization: Basic foo'.split(' '),
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    logging.debug("tmp_mvn_output: %s", test_curl)
+    # test_curl = subprocess.run('curl -X POST https://tomjfrog-pipelines-api.jfrog.io/v1/projectIntegrations/127/hook --header Authorization: Basic foo'.split(' '),
+    url = "https://tomjfrog-pipelines-api.jfrog.io/v1/projectIntegrations/127/hook"
+    payload = ""
+    auth_header_value = f'Basic {basic_auth_string}'
+    headers = {"Authorization": auth_header_value}
+    response = requests.request("POST", url, data=payload, headers=headers)
+    logging.debug("tmp_mvn_output: %s", response)
     logging.info("Finishing Python Script...")
-
 
 if __name__ == "__main__":
     main()
