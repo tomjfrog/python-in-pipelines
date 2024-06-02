80eaf9f660d06185a8cba0daed2ed5ca1c0ef886
tomjfrog <tomj@jfrog.com>
2024-06-02 14:44:10 -0500

    execute python script 2

--- test.py
+++ test.py
@@ -13,8 +13,12 @@ def main():
         format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s: %(message)s",
         level=logging.DEBUG
     )
+    logging.info("Starting Python Script...")
     test_curl = subprocess.run('curl https://www.google.com/'.split(' '),
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logging.debug("tmp_mvn_output: %s", test_curl)
+    tmp_mvn_str = test_curl.stdout.decode()
+    logging.info("Finishing Python Script...")
 
 
 if __name__ == "__main__":
